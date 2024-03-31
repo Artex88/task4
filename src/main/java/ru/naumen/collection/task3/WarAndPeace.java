89f18a26509794b244ecefd3343a1ecd55d6c68e
Alex Zadevalov <zadevalov.alex@gmail.com>
2024-03-31 11:19:46 +0500

    fix third task

--- src/main/java/ru/naumen/collection/task3/WarAndPeace.java
+++ src/main/java/ru/naumen/collection/task3/WarAndPeace.java
@@ -1,8 +1,5 @@
 package ru.naumen.collection.task3;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
@@ -18,102 +15,40 @@ import java.util.*;
  */
 public class WarAndPeace {
 
-    private static class Pair{
-        private final String word;
-        private int count;
+    private static final Path WAR_AND_PEACE_FILE_PATH = Path.of("src/main/resources/WarAndPeace.txt");
 
-        public Pair(String word, int count) {
-            this.word = word;
-            this.count = count;
-        }
-    }
-
-    private static final Path WAR_AND_PEACE_FILE_PATH = Path.of("/home/artex/IdeaProjects/naumen/CollectionsTemplate/src/main/resources/WarAndPeace.txt");
-
+    /**
+     * 1. Использовал LinkedHashMap для эффективного подсчёта слов и дальнейшей итерации по этой коллекции
+     *    и PriorityQueue для поиска 10 наиб. и 10 наим. встречаемых слов.
+     * 2. O(n)
+     * 3. Читаем все слова - O(n). Добавление и сортировка в PriorityQueue происходит за константу, т.к. размер PriorityQueue - константа.
+     */
     public static void main(String[] args) {
-        Map<String, Integer> hashMap = new HashMap<>();
-        Pair[] pairListTop10Small = new Pair[10];
-        Pair[] pairListTop10Big = new Pair[10];
-        SortedMap<Integer, String> stringSortedMap = new TreeMap<>();
-        HashSet<String> top10SmallWords = new HashSet<>();
-
-
-        Comparator<Pair> comparator = Comparator.comparingInt(o -> o.count);
-        var ref = new Object() {
-            int cnt1 = 0;
-            int cnt2 = 0;
-
-            int maxMinimCount = Integer.MAX_VALUE;
-        };
-
-        for (int i = 0; i < 10; i++){
-            pairListTop10Small[i] = new Pair("", Integer.MAX_VALUE);
-            pairListTop10Big[i] = new Pair("", 0);
-        }
+        Map<String, Integer> linkedHashMap = new LinkedHashMap<>();
+        Queue<Map.Entry<String, Integer>> top10Big = new PriorityQueue<>(11, Comparator.comparingInt(Map.Entry::getValue));
+        Queue<Map.Entry<String, Integer>> top10Small = new PriorityQueue<>(11, (a,b) -> b.getValue()  - a.getValue());
 
         WordParser wordParser = new WordParser(WAR_AND_PEACE_FILE_PATH);
+
         wordParser.forEachWord(word -> {
-
-            if (hashMap.containsKey(word))
-                hashMap.put(word, hashMap.get(word) + 1);
-            else
-                hashMap.put(word, 1);
-            int wordCnt = hashMap.get(word);
-
-            if (wordCnt >= pairListTop10Big[0].count){
-                boolean flag = false;
-
-                for (Pair pair : pairListTop10Big) {
-                    if (pair.word.equals(word)) {
-                        flag = true;
-                        pair.count = wordCnt;
-                        break;
-                    }
-                }
-
-                if (flag)
-                    Arrays.sort(pairListTop10Big, comparator);
-                else {
-                    if (ref.cnt1 != 10){
-                        pairListTop10Big[ref.cnt1] = new Pair(word,wordCnt);
-                        ref.cnt1++;
-                    }
-                    else
-                        pairListTop10Big[0] = new Pair(word,wordCnt);
-                }
-
-            }
-
-            if (wordCnt <= pairListTop10Small[9].count || top10SmallWords.contains(word)){
-                boolean flag = false;
-
-                for (Pair pair : pairListTop10Small) {
-                    if (pair.word.equals(word)) {
-                        flag = true;
-                        pair.count = wordCnt;
-                        break;
-                    }
-                }
-                if (!flag) {
-                    if (ref.cnt2 != 10){
-                        pairListTop10Small[ref.cnt2] = new Pair(word,wordCnt);
-                        top10SmallWords.add(word);
-                        ref.cnt2++;
-                    }
-                    else{
-                        top10SmallWords.remove(pairListTop10Small[9].word);
-                        top10SmallWords.add(word);
-                        pairListTop10Small[9] = new Pair(word,wordCnt);
-                    }
-                }
-                Arrays.sort(pairListTop10Small, comparator);
-            }
+            linkedHashMap.put(word, linkedHashMap.getOrDefault(word, 0) + 1);
         });
 
-        for (Pair pair: pairListTop10Big)
-            System.out.println(pair.word + ": " + pair.count);
-        System.out.println("-------------");
-        for (Pair pair: pairListTop10Small)
-            System.out.println(pair.word + ": " + pair.count);
+        for (Map.Entry<String,Integer> entry: linkedHashMap.entrySet()){
+
+            top10Big.add(entry);
+            if (top10Big.size() == 11)
+                top10Big.poll();
+
+            top10Small.add(entry);
+            if (top10Small.size() == 11)
+                top10Small.poll();
+        }
+
+        for (Map.Entry<String, Integer> entry: top10Big)
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        System.out.println("-------------------");
+        for (Map.Entry<String, Integer> entry: top10Small)
+            System.out.println(entry.getKey() + ": " + entry.getValue());
     }
 }
